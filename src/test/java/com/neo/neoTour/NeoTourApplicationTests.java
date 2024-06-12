9dd8ba6b19e76a701da802cd2e303a87cd34e9b4
Nargiza <nargizh03@gmail.com>
2024-06-12 12:40:32 +0500

    First commit.

---
+++ src/test/java/com/neo/neoTour/NeoTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.neoTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
